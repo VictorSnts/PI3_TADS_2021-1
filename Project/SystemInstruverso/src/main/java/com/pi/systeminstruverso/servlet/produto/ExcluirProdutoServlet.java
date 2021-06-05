c02df784efac6a83c01d37d44fd4f64cc9b1c66b
victorsnts <62514371+victorsnts@users.noreply.github.com>
2021-06-05 20:11:54 -0300

    Correcao de um bug que permitia um vendedor apagar um produto da base

--- Project/SystemInstruverso/src/main/java/com/pi/systeminstruverso/servlet/produto/ExcluirProdutoServlet.java
+++ Project/SystemInstruverso/src/main/java/com/pi/systeminstruverso/servlet/produto/ExcluirProdutoServlet.java
@@ -6,12 +6,14 @@
 package com.pi.systeminstruverso.servlet.produto;
 
 import com.pi.systeminstruverso.dao.ProdutoDAO;
+import com.pi.systeminstruverso.entidade.Usuario;
 import com.pi.systeminstruverso.utils.Convert;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -22,21 +24,30 @@ public class ExcluirProdutoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Recuperar os parametros
-        int cod = Convert.ToInt(request.getParameter("cod"));
-                
-        // deleatr o cliente no BD
-        boolean ok = ProdutoDAO.deletar(cod);
-              
-        // Redirecionar para sucesso/erro
-        if (ok) {
-            response.sendRedirect("retornos/sucesso.jsp");
+        
+        HttpSession session = request.getSession();
+        Usuario usuario_logado = (Usuario) session.getAttribute("usuario_logado");
+        
+        if (usuario_logado.getPerfil().equals("BACKOFFICE")) {
+            // Recuperar os parametros
+            int cod = Convert.ToInt(request.getParameter("cod"));
+
+            // deleatr o cliente no BD
+            boolean ok = ProdutoDAO.deletar(cod);
+
+            // Redirecionar para sucesso/erro
+            if (ok) {
+                response.sendRedirect("retornos/sucesso.jsp");
+            }
+            else {
+                String msgErro = "Não foi possivel realizar o exclusao desse produto.";
+                request.setAttribute("msgErro", msgErro);
+                request.getRequestDispatcher("retornos/erro.jsp").forward(request, response);
+            }
+        } else {
+            response.sendRedirect(request.getContextPath() + "/retornos/erro_auth.jsp");
         }
-        else {
-            String msgErro = "Não foi possivel realizar o exclusao desse produto.";
-            request.setAttribute("msgErro", msgErro);
-            request.getRequestDispatcher("retornos/erro.jsp").forward(request, response);
-        }
+        
     }
 
 }
